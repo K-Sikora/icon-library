670df7e78f6b858a001567e0df107eaadfd2c135
K-Sikora <krzysieksikora00123@gmail.com>
2023-07-12 18:00:58 +0200

    imports fix

--- app/services/getIcons.ts
+++ app/services/getIcons.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+import { initializeApp } from "@firebase/app";
+import { getStorage, ref, listAll, getDownloadURL } from "@firebase/storage";
 
 export type Icons = {
   url: string;
