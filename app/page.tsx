5769cfc5c4d3cee1ff4c92bf0ee2b108a6a83ffb
K-Sikora <krzysieksikora00123@gmail.com>
2023-07-16 13:20:54 +0200

    Add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,16 @@ export default async function Home() {
     const allIcons = await getIcons(category.name);
     iconsLength += allIcons.length;
   }
+  const roundedLength = Math.floor(iconsLength / 1000) * 1000;
+
+  const formattedLength = roundedLength.toLocaleString();
 
   return (
     <main className="relative bg-[url('/bg.svg')] flex flex-col items-start justify-center w-full min-h-screen gap-8 px-4 -mt-20 text-white py-36 md:px-24 bg-no-repeat bg-right bg-cover md:bg-contain">
       <h2 className="z-20 text-5xl font-bold">Vector icons library</h2>
 
       <h5 className="z-20 text-lg font-medium md:text-xl">
-        Browse over {iconsLength} icons
+        Browse over {formattedLength} icons
       </h5>
       <div className="z-20 flex flex-wrap items-center gap-4">
         {categories.map((category) => (
